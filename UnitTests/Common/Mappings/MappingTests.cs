d6dee904a39b3da4dec27e2492384b992099d2d7
TaqBostan <mehdi.rajaei87@gmail.com>
2025-08-02 20:23:15 +0330

    Unit Tests

---
+++ UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using CleanDotnetBlazor.Application.Common.Interfaces;
+using CleanDotnetBlazor.Domain.Entities;
+using CleanDotnetBlazor.Shared;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(config =>
+                config.AddMaps(Assembly.GetAssembly(typeof(IApplicationDbContext))));
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Test]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        [TestCase(typeof(Customer), typeof(CustomerDto))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = GetInstanceOf(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+
+        private object GetInstanceOf(Type type)
+        {
+            if (type.GetConstructor(Type.EmptyTypes) != null)
+                return Activator.CreateInstance(type)!;
+
+            // Type without parameterless constructor
+            return RuntimeHelpers.GetUninitializedObject(type);
+        }
+    }
+}
